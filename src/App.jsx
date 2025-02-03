2954010ef8a6fd7d566f5a7de1a4bca2b55b4421
Timur Shomakhov <timur-colmberg@web.de>
2025-02-03 21:12:10 +0100

    Add base layout in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-import Title from "./components/Title";
-import EntryCard from "./components/EntryCard";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home"; 
+import Movies from "./components/Movies";
+import About from "./components/About";
 
-function App() {
+const App = () => {
   return (
-    <>
-      <Title />
-      <EntryCard />
-    </>
+    <div className="min-h-screen flex flex-col">
+      {/* Navbar */}
+      <nav className="bg-blue-600 text-white p-4 shadow-lg">
+        <div className="container mx-auto flex justify-between items-center">
+          <h1 className="text-2xl font-bold">My App</h1>
+          <div className="space-x-4">
+            <Link to="/" className="hover:underline">Home</Link>
+            <Link to="/movies" className="hover:underline">Movies</Link>
+            <Link to="/about" className="hover:underline">About</Link>
+          </div>
+        </div>
+      </nav>
+
+      {/* Page Routing */}
+      <main className="flex-grow container mx-auto p-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+
+      {/* Footer */}
+      <footer className="bg-gray-800 text-white text-center p-4 mt-4">
+        <p>&copy; {new Date().getFullYear()} My App. All rights reserved.</p>
+      </footer>
+    </div>
   );
-}
+};
 
 export default App;
