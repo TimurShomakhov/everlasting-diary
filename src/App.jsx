4a47d036e436ddd56a71a84edb291abe923a044c
Timur Shomakhov <timur-colmberg@web.de>
2025-02-04 16:32:10 +0100

    Replacing app.jsx-code with a correct one

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./components/Home"; 
-import Movies from "./components/Movies";
-import About from "./components/About";
+import Home from "./components/Home";
+import EntryCard from "./components/EntryCard";
+import FotoCard from "./components/FotoCard";
 
 const App = () => {
   return (
     <div className="min-h-screen flex flex-col">
-      {/* Navbar */}
-      <nav className="bg-blue-600 text-white p-4 shadow-lg">
-        <div className="container mx-auto flex justify-between items-center">
-          <h1 className="text-2xl font-bold">My App</h1>
-          <div className="space-x-4">
-            <Link to="/" className="hover:underline">Home</Link>
-            <Link to="/movies" className="hover:underline">Movies</Link>
-            <Link to="/about" className="hover:underline">About</Link>
-          </div>
-        </div>
-      </nav>
-
-      {/* Page Routing */}
+      {/* Page Content */}
       <main className="flex-grow container mx-auto p-4">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/entry" element={<EntryCard />} />
+          <Route path="/photos" element={<FotoCard />} />
         </Routes>
       </main>
 
       {/* Footer */}
       <footer className="bg-gray-800 text-white text-center p-4 mt-4">
-        <p>&copy; {new Date().getFullYear()} My App. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} Everlasting Diary. All rights reserved.</p>
       </footer>
     </div>
   );
