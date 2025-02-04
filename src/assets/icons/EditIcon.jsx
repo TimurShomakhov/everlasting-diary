451e84b8ae7936441766feb7437ce0a0df68bad4
“Aniko” <aniko.juhasz@yahoo.com>
2025-02-04 08:56:45 +0100

    Added SVG icons and JSX components for dynamic styling

---
+++ src/assets/icons/EditIcon.jsx
@@ -0,0 +1,25 @@
+const EditIcon = ({ className }) => (
+    <svg
+        width="38"
+        height="38"
+        viewBox="0 0 38 38"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className={className}>
+        <g clipPath="url(#clip0_5175_345)">
+            <path
+                d="M13.5712 33.2228L1.35693 36.6428L4.77693 24.4285L27.1426 2.1714C27.3953 1.91287 27.6974 1.70746 28.0305 1.56722C28.3638 1.42698 28.7218 1.35474 29.0834 1.35474C29.4449 1.35474 29.8029 1.42698 30.1362 1.56722C30.4693 1.70746 30.7714 1.91287 31.0241 2.1714L35.8284 7.00282C36.0827 7.25514 36.2846 7.55537 36.4225 7.88611C36.5604 8.21687 36.6312 8.57165 36.6312 8.92997C36.6312 9.28828 36.5604 9.64306 36.4225 9.97383C36.2846 10.3046 36.0827 10.6048 35.8284 10.8571L13.5712 33.2228Z"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+        </g>
+        <defs>
+            <clipPath id="clip0_5175_345">
+                <rect width="38" height="38" fill="white" />
+            </clipPath>
+        </defs>
+    </svg>
+);
+
+export default EditIcon;
